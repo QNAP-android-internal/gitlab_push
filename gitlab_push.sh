76ce7be6bd9a32fa7caf334c4808290da7078aae
cmwang <cmwang@ieiworld.com>
2022-09-01 15:22:40 +0800

    Add gl_create_path(), gl_del_path(), and gl_path_id()

--- gitlab_push.sh
+++ gitlab_push.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 TOP=`pwd`
-TOP_DIR="sw3_android_rk356x_group"
+TOP_DIR="sw2_android_rk356x_group"
 
 source ${TOP}/gl_api.sh
 
-declare -A dirids
-gl_list_dir_id dirids "${TOP_DIR}"
-if [ "${#dirids[@]}" -eq "0" ]; then
-    echo "There is no sub dirs under!"
-else
-    for subdir in ${!dirids[@]}; do
-        echo "The id of $subdir is ${dirids[$subdir]}"
-    done
-fi
+function Test_gl_list_dir_id()
+{
+    declare -A dirids
+    gl_list_dir_id dirids "${TOP_DIR}"
+    if [ "${#dirids[@]}" -eq "0" ]; then
+        echo "There is no sub dirs under!"
+    else
+        for subdir in ${!dirids[@]}; do
+            echo "The id of $subdir is ${dirids[$subdir]}"
+        done
+    fi
+}
+
+### This feature needs gitlab version 13.5 or above
+function Test_gl_list_dir_recursive()
+{
+    subdirs=$(gl_list_dir_recursive ${TOP_DIR})
+    if [ "${#subdirs[@]}" -eq "0" ]; then
+        echo "There is no sub dirs under!"
+    else
+        for path in ${subdirs}; do
+            echo "${path}"
+        done
+    fi
+}
+
+function Test_gl_create_path()
+{
+    local path="$1"
+    gl_create_path "$path"
+}
+
+echo "Test functions..."
+#Test_gl_create_path "${TOP_DIR}/platform/build/tools"
+#Tbest_gl_create_path "${TOP_DIR}/platform/build/maketool"
+#Test_gl_create_path "${TOP_DIR}/development/tools"
+#Test_gl_create_path "${TOP_DIR}/device/rk356x"
+
+id=$(gl_path_id "${TOP_DIR}/platform/build")
+echo "Path id is $id"
+gl_del_path "${TOP_DIR}/platform"
+gl_del_path "${TOP_DIR}/development"
+gl_del_path "${TOP_DIR}/device"
--- gl_api.sh
+++ gl_api.sh
@@ -2,6 +2,8 @@ TOKEN="WGj-1hBBVYwZSj7S6Ts-"
 URL="https://gitsrv-android.ieiworld.com"
 GITLAB_API="${URL}/api/v4"
 
+# INPUT: $1 as directory
+# OUTPUT: get the directory(group) id of $1 dir
 function gl_dir_id()
 {
     local dir=$1
@@ -14,9 +16,18 @@ function gl_dir_id()
     fi
 }
 
-# INPUT: $1 an associative array with path as the key and id the value
-#        $2 directory
-# OUTPUT: list sub directories and their ids under dir
+# INPUT: $1 as path
+# OUTPUT: get the dir id of $1 path
+function gl_path_id()
+{
+    local path=$1
+    local query=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" --request GET --url "${GITLAB_API}/groups")
+    local id=$(echo $query | jq --argjson i "[\"$path\"]" '.[]|select(.full_path==$i[])|.id')
+    echo $id
+}
+
+# INPUT: $1 directory
+# OUTPUT: list sub directories under $1 dir
 function gl_list_dir()
 {
     local dir=$1
@@ -29,26 +40,90 @@ function gl_list_dir()
     fi
 }
 
+# INPUT: $1 directory
+# OUTPUT: list sub directories recursively under $1 dir
+# This API need Gitlab version 13.5 or above
+function gl_list_dir_recursive()
+{
+    local dir=$1
+    local dir_id=$(gl_dir_id "$dir")
+
+    if [ ! -z "$dir_id" ]; then
+	local query=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" -XGET "${GITLAB_API}/groups/${dir_id}/descendant_groups")
+	local sub_dirs=$(echo $query | jq '.[].path')
+	echo $sub_dirs
+    fi
+}
+
 # INPUT: $1 an associative array with path as the key and id the value
 #        $2 directory
-# OUTPUT: list sub directories and their ids under dir
+# OUTPUT: list sub directories and their ids under $2 dir
 function gl_list_dir_id()
 {
-    declare -n dirids_array="$1"
+    local -n dirids_array="$1"
     local dir=$2
     local dir_id=$(gl_dir_id "$dir")
 
     if [ ! -z "$dir_id" ]; then
 	local query=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" -XGET "${GITLAB_API}/groups/${dir_id}/subgroups")
-	#declare -A sub_dirs=$(echo $query | jq -r '.[].path as $paths|@sh "[\($paths)]=\(map(select(.path == $paths).id))"')
+        #dirids_array="($(echo $query | jq -r '.[].path as $paths|@sh "[\($paths)]=\(map(select(.path == $paths).id)|join(" "))"'))"
+	#echo "${#dirids_arry[@]}"
         while IFS="=" read -r key value
         do
-            dirids_array[$key]=$value
+            dirids_array["$key"]=$value
 	done < <(echo $query | jq -r '.[].path as $paths|@sh "\($paths)=\(map(select(.path == $paths).id))"')
     fi
 }
 
+# INPUT: $1 as path. eg. foo/bar will be created respectively
+# OUTPUT: an associative array of path name and its id
 function gl_create_path()
 {
-    echo $0
+    local path=$1
+    local components
+    IFS="/" components=( $path )
+
+    for dir in "${components[@]}"; do
+        local dir_id=$(gl_path_id "$dir")
+	local parent_dir_id
+	if [ ! -z "$dir_id" ]; then
+            parent_dir_id="$dir_id"
+        else
+	    local json_data="{\"name\": \"${dir}\", \"path\": \"${dir}\", \"visibility\": \"internal\", \"description\": \"${dir}\"}"
+            parent_dir_id=$(curl -s --request POST --header "PRIVATE-TOKEN: $TOKEN" --header "Content-Type: application/json" \
+		            --data "$json_data" \
+		            "${GITLAB_API}/groups?parent_id=$parent_dir_id" | jq '.id')
+            unset dir_id
+        fi
+    done
+}
+
+# INPUT: $1 as path. Delete the dir recursively. eg. foo/bar deleting everything under directory bar including bar itself.
+# OUTPUT: null
+function gl_del_path()
+{
+    local path=$1
+    local id=$(gl_path_id "$path")
+
+    if [ ! -z "$id" ]; then
+	local result=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" --request DELETE --url "${GITLAB_API}/groups/${id}")
+    fi    
+}
+
+# INPUT: $1 as path. eg. foo/bar. the last component of path is
+#        regarded as the project name.
+# OUTPUT: an associative array of dir/project name and their ids
+function gl_create_project()
+{
+    local path="$1"
+    local dir=$(dirname "$path")
+    local project=$(basename "$path")
+
+    if [ ! -z "$dir" ]; then
+        gl_create_path $dir
+    fi	
+
+    if [ ! -z "$project" ]; then
+       PROJECT1_ID=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" -XPOST "${GITLAB_API}/projects?name=MyCFProject&visibility=private&namespace_id=$SUB_GROUP1_ID" | jq '.id') 
+    fi	
 }
